5c2a10c3f08f25e19729e553f67a70af82fe88f1
André Soares <37581899+andrevms@users.noreply.github.com>
2024-10-18 21:50:37 -0300

    Fix return type

--- streaming/src/main/java/br/com/pds/streaming/authentication/services/UserService.java
+++ streaming/src/main/java/br/com/pds/streaming/authentication/services/UserService.java
@@ -1,8 +1,8 @@
 package br.com.pds.streaming.authentication.services;
 
+import br.com.pds.streaming.authentication.models.entities.User;
 import br.com.pds.streaming.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +11,7 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public UserDetails loadUserByUsername(String username) {
+    public User loadUserByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 }
