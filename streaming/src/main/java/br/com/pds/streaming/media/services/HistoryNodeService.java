acc93986aaf8c603ea2cfba39899914ba9288119
renzozuk <renzo.zuk19@gmail.com>
2024-11-23 20:01:32 -0300

    Fix: An adjustment was made in HistoryNodeService

--- streaming/src/main/java/br/com/pds/streaming/media/services/HistoryNodeService.java
+++ streaming/src/main/java/br/com/pds/streaming/media/services/HistoryNodeService.java
@@ -3,10 +3,14 @@ package br.com.pds.streaming.media.services;
 import br.com.pds.streaming.exceptions.ObjectNotFoundException;
 import br.com.pds.streaming.mapper.modelMapper.MyModelMapper;
 import br.com.pds.streaming.media.model.dto.HistoryNodeDTO;
+import br.com.pds.streaming.media.model.entities.Episode;
 import br.com.pds.streaming.media.model.entities.History;
 import br.com.pds.streaming.media.model.entities.HistoryNode;
+import br.com.pds.streaming.media.model.entities.Movie;
+import br.com.pds.streaming.media.repositories.EpisodeRepository;
 import br.com.pds.streaming.media.repositories.HistoryNodeRepository;
 import br.com.pds.streaming.media.repositories.HistoryRepository;
+import br.com.pds.streaming.media.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +21,16 @@ public class HistoryNodeService {
 
     private final HistoryNodeRepository historyNodeRepository;
     private final HistoryRepository historyRepository;
+    private final EpisodeRepository episodeRepository;
+    private final MovieRepository movieRepository;
     private final MyModelMapper mapper;
 
     @Autowired
-    public HistoryNodeService(HistoryNodeRepository historyNodeRepository, HistoryRepository historyRepository, MyModelMapper mapper) {
+    public HistoryNodeService(HistoryNodeRepository historyNodeRepository, HistoryRepository historyRepository, EpisodeRepository episodeRepository, MovieRepository movieRepository, MyModelMapper mapper) {
         this.historyNodeRepository = historyNodeRepository;
         this.historyRepository = historyRepository;
+        this.episodeRepository = episodeRepository;
+        this.movieRepository = movieRepository;
         this.mapper = mapper;
     }
 
@@ -47,11 +55,33 @@ public class HistoryNodeService {
         return mapper.convertValue(historyNode, HistoryNodeDTO.class);
     }
 
-    public HistoryNodeDTO insert(HistoryNodeDTO historyNodeDTO, String historyId) throws ObjectNotFoundException {
+    public HistoryNodeDTO insert(String episodeId, HistoryNodeDTO historyNodeDTO, String historyId) throws ObjectNotFoundException {
+
+        var episode = episodeRepository.findById(episodeId).orElseThrow(() -> new ObjectNotFoundException(Episode.class));
 
         var history = historyRepository.findById(historyId).orElseThrow(() -> new ObjectNotFoundException(History.class));
 
         var historyNode = mapper.convertValue(historyNodeDTO, HistoryNode.class);
+        historyNode.setWatchable(episode);
+        historyNode.setHistoryId(historyId);
+
+        var createdHistoryNode = historyNodeRepository.save(historyNode);
+
+        history.getNodes().add(createdHistoryNode);
+
+        historyRepository.save(history);
+
+        return mapper.convertValue(createdHistoryNode, HistoryNodeDTO.class);
+    }
+
+    public HistoryNodeDTO insert(HistoryNodeDTO historyNodeDTO, String movieId, String historyId) throws ObjectNotFoundException {
+
+        var movie = movieRepository.findById(movieId).orElseThrow(() -> new ObjectNotFoundException(Movie.class));
+
+        var history = historyRepository.findById(historyId).orElseThrow(() -> new ObjectNotFoundException(History.class));
+
+        var historyNode = mapper.convertValue(historyNodeDTO, HistoryNode.class);
+        historyNode.setWatchable(movie);
         historyNode.setHistoryId(historyId);
 
         var createdHistoryNode = historyNodeRepository.save(historyNode);
